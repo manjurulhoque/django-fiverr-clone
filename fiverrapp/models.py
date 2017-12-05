7bbe722cc28510e07bbf63c2b06d2919d0541e88
manjurulhoque <rumimanzurulhoque@gmail.com>
2017-12-05 12:54:56 +0600

    models created and migrated

--- fiverrapp/models.py
+++ fiverrapp/models.py
@@ -1,3 +1,55 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.CharField(max_length=500)
+    about = models.CharField(max_length=1000)
+    slogan = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Gig(models.Model):
+    CATEGORY_CHOICES = (
+        ("GD", "Graphics & Design"),
+        ("DM", "Digital & Marketing"),
+        ("VA", "Video & Animation"),
+        ("MA", "Music & Audio"),
+        ("PT", "Programming & Tech")
+    )
+
+    title = models.CharField(max_length=500)
+    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
+    description = models.CharField(max_length=1000)
+    price = models.IntegerField(default=6)
+    photo = models.FileField(upload_to='gigs')
+    status = models.BooleanField(default=True)
+    user = models.ForeignKey(User)
+    create_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class Purchase(models.Model):
+    gig = models.ForeignKey(Gig)
+    buyer = models.ForeignKey(User)
+    time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.gig.title
+
+
+class Review(models.Model):
+    gig = models.ForeignKey(Gig)
+    user = models.ForeignKey(User)
+    content = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.content
--- fiverrapp/urls.py
+++ fiverrapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_detail, name='gig_detail'),
+    # url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_detail, name='gig_detail'),
 ]
