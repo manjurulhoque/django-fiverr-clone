e93fa125997341d52b8543a5fa07984854831dc0
manjurulhoque <rumimanzurulhoque@gmail.com>
2017-12-05 20:33:11 +0600

    fixing navbar and buy sell

--- fiverrapp/urls.py
+++ fiverrapp/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from .views import (home, login_view, register_view, logout_view, create_gig, my_gigs, edit_gig, gig_detail, profile,
-                    create_purchase)
+                    create_purchase, my_sellings, my_buyings)
 
 urlpatterns = [
     url(r'^$', home, name='home'),
@@ -13,4 +13,7 @@ urlpatterns = [
     url(r'^gigs/(?P<id>[0-9]+)/$', gig_detail, name='gig_detail'),
     url(r'^profile/(?P<username>\w+)/$', profile, name='profile'),
     url(r'^checkout/$', create_purchase, name='create_purchase'),
+    url(r'^my_sellings/$', my_sellings, name='my_sellings'),
+    url(r'^my_buyings/$', my_buyings, name='my_buyings'),
+
 ]
